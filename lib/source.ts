82d818dafacd6102736cad9d560a7fe773c32274
Nasfame <laciferin2024@protonmail.com>
2025-07-11 04:21:41 +0400

    openai spec integrate

--- lib/source.ts
+++ lib/source.ts
@@ -1,9 +1,21 @@
-import { docs } from '@/.source';
-import { loader } from 'fumadocs-core/source';
+// import { docs } from '@/.source';
+// import { loader } from 'fumadocs-core/source';
 
-// See https://fumadocs.vercel.app/docs/headless/source-api for more info
+// // See https://fumadocs.vercel.app/docs/headless/source-api for more info
+// export const source = loader({
+//   // it assigns a URL to your pages
+//   baseUrl: '/docs',
+//   source: docs.toFumadocsSource(),
+// });
+
+import { createOpenAPI, attachFile } from "fumadocs-openapi/server"
+import { loader } from "fumadocs-core/source"
+
 export const source = loader({
-  // it assigns a URL to your pages
-  baseUrl: '/docs',
-  source: docs.toFumadocsSource(),
-});
+  pageTree: {
+    // adds a badge to each page item in page tree
+    attachFile,
+  },
+})
+
+export const openapi = createOpenAPI()
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,12 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import defaultComponents from "fumadocs-ui/mdx"
+import { APIPage } from "fumadocs-openapi/ui"
+import { openapi } from "@/lib/source"
+import type { MDXComponents } from "mdx/types"
 
-// use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
-    ...defaultMdxComponents,
+    ...defaultComponents,
+    APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
     ...components,
-  };
+  }
 }
