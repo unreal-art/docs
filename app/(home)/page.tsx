11c1f68e5bed0bb526b1907c764692458c70c340
Nasfame <laciferin2024@protonmail.com>
2025-07-11 04:12:31 +0400

    ready:

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation"
 
 export default function HomePage() {
-  redirect('/api');
-  return null;
+  redirect("/docs/api")
+  return null
 }
