571d1a826ba592c8faa4a2505d69c9777eba1cc0
Nasfame <laciferin2024@protonmail.com>
2025-07-11 04:44:44 +0400

    wip:

--- lib/source.ts
+++ lib/source.ts
@@ -1,21 +1,12 @@
-// import { docs } from '@/.source';
-// import { loader } from 'fumadocs-core/source';
-
-// // See https://fumadocs.vercel.app/docs/headless/source-api for more info
-// export const source = loader({
-//   // it assigns a URL to your pages
-//   baseUrl: '/docs',
-//   source: docs.toFumadocsSource(),
-// });
-
-import { createOpenAPI, attachFile } from "fumadocs-openapi/server"
+import { docs } from "@/.source"
 import { loader } from "fumadocs-core/source"
+import { createOpenAPI } from "fumadocs-openapi/server"
 
+// See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
-  pageTree: {
-    // adds a badge to each page item in page tree
-    attachFile,
-  },
+  // it assigns a URL to your pages
+  baseUrl: "/docs",
+  source: docs.toFumadocsSource(),
 })
 
 export const openapi = createOpenAPI()
--- scripts/gen-docs.mjs
+++ scripts/gen-docs.mjs
@@ -2,7 +2,7 @@ import { generateFiles } from "fumadocs-openapi"
 void generateFiles({
   // the OpenAPI schema, you can also give it an external URL.
   input: ["https://openai.unreal.art/openapi.json"],
-  output: "./content/docs",
+  output: "./content/docs/api",
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
   includeDescription: true,
