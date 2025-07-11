c6302b7e101eac3fdb0c2bf3454f118383deeaba
Nasfame <laciferin2024@protonmail.com>
2025-07-11 04:26:30 +0400

    bun: generatefile script

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -1,7 +1,7 @@
 import { generateFiles } from "fumadocs-openapi"
 void generateFiles({
   // the OpenAPI schema, you can also give it an external URL.
-  input: ["./unkey.json"],
+  input: ["https://openai.unreal.art/openapi.json"],
   output: "./content/docs",
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
