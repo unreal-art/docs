9f22fb910e0ef82cf2dc1294a209b3b8b6837bc5
Nasfame <laciferin2024@protonmail.com>
2025-07-13 08:29:46 +0400

    add: urls

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,17 @@
 export const OPENAI_URL =
   process.env.NEXT_PUBLIC_OPENAI_URL || "https://openai.unreal.art"
+
+export const CONSOLE_URL =
+  process.env.NEXT_PUBLIC_CONSOLE_URL || "https://console.unreal.art"
+
+export const COCKPIT_URL =
+  process.env.NEXT_PUBLIC_COCKPIT_URL || "https://cockpit.unreal.art"
+
+export const UNREAL_URL =
+  process.env.NEXT_PUBLIC_UNREAL_URL || "https://unreal.art"
+
+export const DOCS_URL =
+  process.env.NEXT_PUBLIC_DOCS_URL || "https://docs.unreal.art"
+
+export const STUDIO_URL =
+  process.env.NEXT_PUBLIC_STUDIO_URL || "https://studio.unreal.art"
