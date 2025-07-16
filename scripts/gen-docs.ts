2e5a9ff51350f8e37289a7c4c5060ec606db9e75
Nasfame <laciferin2024@protonmail.com>
2025-07-16 21:23:23 +0400

    gen: docs script updated

--- scripts/gen-docs.ts
+++ scripts/gen-docs.ts
@@ -5,7 +5,7 @@ void generateFiles({
     // "https://openai.unreal.art/openapi.json"
     "./openai.spec.json",
   ],
-  output: "./content/docs/openai",
+  output: "./docs/openai",
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
   includeDescription: true,
