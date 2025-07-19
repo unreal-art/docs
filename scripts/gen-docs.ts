6e27dfcaace166f6e77cf8a1d266b25eac641be5
Nasfame <laciferin2024@protonmail.com>
2025-07-19 17:47:59 +0400

    try: to get things running on vercel

--- scripts/gen-docs.ts
+++ scripts/gen-docs.ts
@@ -1,14 +1,22 @@
 import { generateFiles } from "fumadocs-openapi"
+
+import { rimraf } from "rimraf"
+
+const p = "content/openai"
+
+// rimraf.sync(p) //FIXME: can't do it cuz of custom content
+
 void generateFiles({
   // the OpenAPI schema, you can also give it an external URL.
   input: [
     // "https://openai.unreal.art/openapi.json"
     "./openai.spec.json",
   ],
-  output: "./content/openai",
+  output: p,
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
   includeDescription: true,
   // FIXME: https://github.com/fuma-nama/fumadocs/issues/2085
   // groupBy: "route",
+  per: "operation",
 })
